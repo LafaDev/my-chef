36bfa7385bd27a11b5a9729714406335a3e0072d
Gero <gerson_henrique@pop-os.localdomain>
2022-03-10 11:50:38 -0300

    feat : explore page make

--- src/pages/Explorer.js
+++ src/pages/Explorer.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import LowerMenu from '../components/LowerMenu/LowerMenu';
+import '../styles/Explorer.css';
 
 export default function Explorer() {
   return (
     <section>
       <Header title="Explorer" />
+      <section className="explore">
+        <Link to="/explore/foods">
+          <button
+            type="button"
+            data-testid="explore-foods"
+          >
+            Explore Foods
+          </button>
+        </Link>
+
+        <Link to="/explore/drinks">
+          <button
+            type="button"
+            data-testid="explore-drinks"
+          >
+            Explore Drinks
+          </button>
+        </Link>
+      </section>
       <LowerMenu />
     </section>
   );
